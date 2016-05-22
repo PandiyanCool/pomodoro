9bcab3a353d0f8cdd783bf7346c6fa584da71499
Purushoth <purushothanbu@gmail.com>
2016-05-22 10:37:52 +0530

    Initial commit

---
+++ src/app/pomodoro/pomodoro.controller.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app.pomodoro')
+        .controller('PomodoroController', PomodoroController);
+
+  
+    function PomodoroController() {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
